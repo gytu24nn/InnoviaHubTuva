5d617a2e10e905fa11f5e3c3273ec43247654605
kittzor <kjellgrenchristoffer@gmail.com>
2025-09-08 15:11:34 +0200

    added a signin page, with link to signup page

--- FrontEnd/vite-project/src/Pages/SignIn/SignIn.tsx
+++ FrontEnd/vite-project/src/Pages/SignIn/SignIn.tsx
@@ -1,6 +1,43 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+
 const SignIn = () => {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+    console.log("Inloggning:", { email, password })
+    // Hantera inloggning här
+  }
+
   return (
-    <div>SignIn</div>
+    <div className="signing-container">
+      <div className="signing-box">
+        <h2>Logga in</h2>
+        <form onSubmit={handleSubmit} className="signing-form">
+          <input 
+            type="email" 
+            placeholder="E-post" 
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required 
+          />
+          <input 
+            type="password" 
+            placeholder="Lösenord"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <button type="submit">Logga in</button>
+        </form>
+      </div>
+       <div className="signup-redirect">
+      <p>Har du inget konto? <Link to="/signup">Skapa ett konto</Link></p>
+    </div>
+    </div>
   )
 }
 
